cec8cff885a403b34fa4ab31121c45cbcfc1e728
AntoniaFrck <122741069+AntoniaFrck@users.noreply.github.com>
2025-01-27 21:43:07 +0100

    Update evaluate.py

--- segmentation_package/cli/evaluate.py
+++ segmentation_package/cli/evaluate.py
@@ -9,13 +9,7 @@ if __name__ == "__main__":
     data_module.setup()
 
     # Wybierz model
-    checkpoint_path = "lightning_logs/version_8/checkpoints/epoch=8-val_loss=0.34-val_BinaryF1Score=0.76.ckpt"
-    #checkpoint_path = "lightning_logs/version_6/checkpoints/epoch=8-val_loss=0.23-val_BinaryF1Score=0.77.ckpt"
-    # checkpoint_path = "lightning_logs/version_9/checkpoints/epoch=67-val_loss=0.19-val_BinaryF1Score=0.81.ckpt"
-    #checkpoint_path = "lightning_logs/version_10/checkpoints/epoch=9-val_loss=0.24-val_BinaryF1Score=0.79.ckpt"
-    # checkpoint_path = "lightning_logs/version_15/checkpoints/epoch=27-val_loss=0.17-val_BinaryF1Score=0.84.ckpt"
-    #checkpoint_path = "lightning_logs/version_17/checkpoints/epoch=49-val_loss=0.21-val_BinaryF1Score=0.81.ckpt"
-    #checkpoint_path = "lightning_logs/version_19/checkpoints/epoch=170-val_loss=0.15-val_BinaryF1Score=0.85.ckpt"
+    checkpoint_path = "lightning_logs/version_19/checkpoints/epoch=170-val_loss=0.15-val_BinaryF1Score=0.85.ckpt"
     model = LitModel.load_from_checkpoint(checkpoint_path).eval().cpu()
     x = torch.rand(1, 3, 256, 256)
     _ = model(x)
